b465e2dcff0182c6c46e3c9084a4149c53d3f6bf
nabilmuafa <nmuafa@gmail.com>
2025-02-27 23:33:20 +0700

    add custom gravity for jump

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
-@export var gravity = 200.0
-@export var walk_speed = 200
-@export var jump_speed = -300
+@export var jump_height : float
+@export var jump_time_to_peak : float
+@export var jump_time_to_descent : float
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1
+
+@export var walk_speed = 500
 
 func _physics_process(delta):
-	velocity.y += delta * gravity
+	velocity.y += get_custom_gravity() * delta
 	
 	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
-		velocity.y = jump_speed
+		velocity.y = jump_velocity
 	
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -walk_speed
@@ -18,11 +24,13 @@ func _physics_process(delta):
 		velocity.x = 0
 		
 	move_and_slide()
+	
+func get_custom_gravity() -> float:
+	return jump_gravity if velocity.y > 0.0 else fall_gravity
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
